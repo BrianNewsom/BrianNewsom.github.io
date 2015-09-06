5584763b673f58e7f5e42caa5d9543cd8620f244
Brian Newsom <brian.newsom@colorado.edu>
2015-09-06 00:23:12 -0600

    Complete overhaul of site - in progress

---
+++ js/social.js
@@ -0,0 +1,4 @@
+// add extra elements to buttons for hover, keeps html cleaner
+$('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span></div>');
+
+$('.social-btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span></div>');
